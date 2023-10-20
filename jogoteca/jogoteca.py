4092b1bd67f5db2a788fa58d327fa7344abf0698
Guilherme Lourenço <53027213+emersontop@users.noreply.github.com>
2023-10-20 08:32:10 -0300

    agora podemos deletar um jogo, alterar um jogo e fazer o upload de uma imagem

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -1,96 +1,16 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for
-
-class Jogo(object):
-
-    def __init__(self, nome, categoria, console):
-        self.__nome = nome
-        self.__categoria = categoria
-        self.__console  = console
-
-    @property
-    def nome(self):
-        return self.__nome
-    
-    @property
-    def categoria(self):
-        return self.__categoria
-    
-    @property
-    def console(self):
-        return self.__console
-
-
-jogo1 = Jogo('Tetris','Puzzle','Atari')
-jogo2 = Jogo('God of war','Rack in Slash','PS2')
-jogo3 = Jogo('Mortal combat','Luta','PS2')
-
-lista_de_jogos = [jogo1, jogo2, jogo3]
-
-class Usuario:
-    def __init__(self, nome, nickname, senha):
-        self.nome = nome
-        self.nickname = nickname
-        self.senha = senha
-
-usuario1 = Usuario("emerson", "BD", "alohomora")
-usuario2 = Usuario("Camila Ferreira", "Mila", "paozinho")
-usuario3 = Usuario("Guilherme Louro", "Cake", "Python_eh_vida")
-
-usuarios = { usuario1.nickname : usuario1,
-             usuario2.nickname : usuario2,
-             usuario3.nickname : usuario3 }
+from flask_sqlalchemy import SQLAlchemy
 
 # instancia a aplicação
 app = Flask(__name__)
-app.secret_key ='alura'
 
-# Rota
-@app.route('/')
-def index():
-    # instanciando jogos:
+app.config.from_pyfile('config.py')
 
-    return render_template('lista.html', titulo = 'Jogos', jogos = lista_de_jogos)
+db = SQLAlchemy(app) 
 
-@app.route('/novo')
-def novo():
-    if 'usuario_logado' not in session or session['usuario_logado'] == None:
-        return redirect(url_for('login',proxima = url_for('novo')))
-    return render_template('novo.html', titulo='Novo Jogo')
-
-@app.route('/criar', methods=['POST',])
-def criar():
-    #captura as informações do forms
-    nome = request.form['nome']
-    categoria = request.form['categoria']
-    console = request.form['console']
-    jogo = Jogo(nome, categoria, console)
-    lista_de_jogos.append(jogo)
-    return redirect(url_for('index'))
-
-@app.route('/login')
-def login():
-    proxima = request.args.get('proxima')
-    return render_template('login.html', titulo='Login',proxima = proxima)
-
-@app.route('/autenticar', methods=['POST',])
-def autenticar():
-    if request.form['usuario'] in usuarios:
-        print(request.form['usuario'])
-        usuario = usuarios[request.form['usuario']]
-        if request.form['senha'] == usuario.senha:
-            session['usuario_logado'] = usuario.nickname
-            flash(usuario.nickname + ' logado com sucesso!')
-            proxima_pagina = request.form['proxima']
-            return redirect(proxima_pagina)
-    else:
-        flash('Usuário não logado')
-        return redirect(url_for('login'))
-
-@app.route('/logout')
-def logout():
-    session['usuario_logado'] = None
-    flash('Logout efetuado com sucesso!')
-    return redirect(url_for('index'))
+from views import *
 
 # Roda a a plicação
-app.run(debug = True)
+
+if __name__ == '__main__':
+    app.run(debug = True)
