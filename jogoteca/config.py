ee8b772df9d83e9a0715e244e47d43f13ccc9366
Guilherme Lourenço <53027213+emersontop@users.noreply.github.com>
2023-10-23 09:48:05 -0300

    Atualização dos estudos

--- jogoteca/config.py
+++ jogoteca/config.py
@@ -1,3 +1,5 @@
+import os 
+
 SECRET_KEY ='alura'
 SQLALCHEMY_DATABASE_URI = '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
     SGBD = 'mysql+mysqlconnector',
@@ -6,3 +8,5 @@ SQLALCHEMY_DATABASE_URI = '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.for
     servidor = 'localhost',
     database = 'jogoteca'
     )
+
+UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/upload'
